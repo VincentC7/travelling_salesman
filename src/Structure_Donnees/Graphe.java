fbbd87dc17792f3e7c46a38a3a1fc3c0ae5b849a
noirotquentin <quentin.noir@gmail.com>
2019-11-26 16:51:29 +0100

    Ajout graphe

--- src/Structure_Donnees/Graphe.java
+++ src/Structure_Donnees/Graphe.java
@@ -1,4 +1,30 @@
 package Structure_Donnees;
 
+import java.io.File;
+import java.util.HashSet;
+
 public class Graphe {
+    private HashSet<Ville> villes;
+    private Ville ville_depart;
+
+
+    public HashSet<Ville> getVilles() {
+        return villes;
+    }
+
+    public void setVilles(HashSet<Ville> villes) {
+        this.villes = villes;
+    }
+
+    public Ville getVille_depart() {
+        return ville_depart;
+    }
+
+    public void setVille_depart(Ville ville_depart) {
+        this.ville_depart = ville_depart;
+    }
+
+    static Graphe creer_graphe(File fichier){
+        
+    }
 }
